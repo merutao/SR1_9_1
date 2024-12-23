92ad057b32e1d2bcae93a50a45f2cc20df8c4db2
ruka <115062996+merutao@users.noreply.github.com>
2024-12-23 16:46:18 +0900

    プロジェクト ファイルを追加します。

---
+++ SR1_9_1/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_9_1
+{
+    internal class Item
+    {
+        protected string name;
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public virtual void Use()
+        {
+            Console.WriteLine("{0}を使った", name);
+        }
+    }
+}
---
+++ SR1_9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item("つるはし");
+
+            item.Use();
+
+            Console.ReadLine();
+        }
+    }
+}
