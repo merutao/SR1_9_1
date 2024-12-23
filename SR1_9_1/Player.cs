1c236615f96e3935ced02ef192be7eede9ba4edc
ruka <115062996+merutao@users.noreply.github.com>
2024-12-23 17:01:49 +0900

    9_2

---
+++ SR1_9_1/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR1_9_1
+{
+    internal class Player
+    {
+        Item useItem;
+
+        public Player(Item item) 
+        {
+            useItem = item;
+        }
+
+        public void UseItem()
+        {
+            useItem.Use();
+        }
+    }
+}
